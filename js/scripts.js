2fb735e9de19811f91faac54e7873fa9296d70d7
ZakharchukA <zaharchyk83@gmail.com>
2022-08-18 21:18:52 +0300

    after two lessons - js and adaptacion

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:5
+            }
+        }
+    });
+});
